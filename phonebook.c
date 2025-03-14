62c114190652e01ffca08110b82f4ee9fb1cc814
Ricardo Franco <ricardofrancosantos@gmail.com>
2025-03-13 21:53:14 -0300

    Feat: phonebook exercise refactored using struct

--- phonebook.c
+++ phonebook.c
@@ -2,24 +2,37 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+
+{
+    string name;
+    string number;
+
+} person;
+
 int main (void)
 {
-    string names[] = {"John", "David", "Yuliia"};
-    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750", "+1-617-495-1000"};
+     person people[3];
 
-    string name = get_string("Name: ");
-    for (int i = 0; i < 3; i++)
+     people[0].name = "David";
+     people[0].number = "+1-617-495-1000";
 
-    {
-        if (strcmp(names [i], name) == 0)
+     people[1].name = "John";
+     people[1].number = "+1-949-468-2750";
+
+     people[2].name = "Yuliia";
+     people[2].number = "+1-617-495-1000";
+
+     string name = get_string("Name: ");
+     for (int i = 0; i < 3; i++)
+     {
+        if (strcmp(people[i].name, name) == 0)
         {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
-
-    }
-
-
-printf("Not found\n");
-return 1;
+     }
+     printf("Not Found\n");
+            return 1;
 }
+
